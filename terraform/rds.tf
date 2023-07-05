94fa2780c6695f3e2d1de90fd230214a80d4cbc6
markbiol <88555492+markbiol@users.noreply.github.com>
2023-07-05 11:46:43 +0800

    Update and rename README.md to terraform/rds.tf

---
+++ terraform/rds.tf
@@ -0,0 +1,10 @@
+# simpleenv
+resource "aws_rds_cluster" "app1-rds-cluster" {
+  cluster_identifier      = "app1-rds-cluster"
+  allocated_storage       = 10
+  backup_retention_period = 0
+  tags = {
+    Name = "app1-rds-cluster"
+    Environment = "prod"
+  }
+}
